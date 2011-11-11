7addc8cb1ee0ed8fc1a2d11277ceac616d203998
Peter Denison <angband@marshadder.org>
2011-11-11 22:02:11 +0000

    Borg: switch to using struct object_kind * instead of k_idx (almost) throughout Remove special check for Palantir, which a) no longer exists, and b) won't   work now anyway, as artifact special kinds are dynamically created Change kind ranges for gold into check for TV_GOLD, and remove K_MONEY_*
    Signed-off-by: Peter Denison <angband@marshadder.org>

--- src/borg/borg3.c
+++ src/borg/borg3.c
@@ -2047,6 +2047,7 @@ void borg_item_analyze(borg_item *item, object_type *real_item, cptr desc)
 
     /* XXX XXX XXX Repair various "ego-items" */
 
+#if 0
 	/* Repair the Planatir of Westerness. The borg thinks
 	 * it is an ego item.since it has an ego name (Westerness).
 	 */
@@ -2059,7 +2060,6 @@ void borg_item_analyze(borg_item *item, object_type *real_item, cptr desc)
 		item->pval = 2;
 	}
 
-#if 0
 	/* Repair the Shield of the Haradrim. The borg thinks
 	 * it is an ego item.since it has an ego name (Haradrim).
 	 */
@@ -4043,7 +4043,7 @@ void borg_cheat_equip(void)
     for (i = INVEN_WIELD; i < ALL_INVEN_TOTAL; i++)
     {
 		/* skip non items */
-		if (!p_ptr->inventory[i].kind->kidx)
+		if (!p_ptr->inventory[i].kind)
 		{
 			/* Be sure to wipe it from the borg equip */
 			WIPE(&borg_items[i], borg_item);
@@ -4053,24 +4053,24 @@ void borg_cheat_equip(void)
         buf[0] = '\0';
 
         /* Describe a real item */
-        if (p_ptr->inventory[i].kind->kidx)
+        if (p_ptr->inventory[i].kind)
         {
             /* Describe it */
             object_desc(buf, sizeof(buf), &p_ptr->inventory[i], ODESC_FULL);
+
+			/* Analyze the item (no price) */
+			borg_item_analyze(&borg_items[i], &p_ptr->inventory[i], buf);
+
+			/* get the fully id stuff */
+			if (p_ptr->inventory[i].ident & (IDENT_KNOWN | IDENT_EFFECT))
+			{
+				borg_items[i].fully_identified = TRUE;
+				borg_items[i].needs_I = FALSE;
+			}
+
+			/* Uninscribe items with ! inscriptions */
+			if (strstr(borg_items[i].desc, "!")) borg_send_deinscribe(i);
         }
-
-        /* Analyze the item (no price) */
-        borg_item_analyze(&borg_items[i], &p_ptr->inventory[i], buf);
-
-        /* get the fully id stuff */
-        if (p_ptr->inventory[i].ident & (IDENT_KNOWN | IDENT_EFFECT))
-        {
-            borg_items[i].fully_identified = TRUE;
-            borg_items[i].needs_I = FALSE;
-    	}
-
-		/* Uninscribe items with ! inscriptions */
-		if (strstr(borg_items[i].desc, "!")) borg_send_deinscribe(i);
     }
 }
 
@@ -4091,7 +4091,7 @@ void borg_cheat_inven(void)
     for (i = 0; i < INVEN_MAX_PACK; i++)
     {
 		/* Skip non-items */
-		if (!p_ptr->inventory[i].kind->kidx)
+		if (!p_ptr->inventory[i].kind)
 		{
 			/* Wipe from borg lists */
 			WIPE(&borg_items[i], borg_item);
@@ -4100,12 +4100,8 @@ void borg_cheat_inven(void)
         /* Default to "nothing" */
         buf[0] = '\0';
 
-        /* Describe a real item */
-        if (p_ptr->inventory[i].kind->kidx)
-        {
-            /* Describe it */
-            object_desc(buf, sizeof(buf), &p_ptr->inventory[i], ODESC_FULL);
-        }
+        /* Describe it */
+        object_desc(buf, sizeof(buf), &p_ptr->inventory[i], ODESC_FULL);
 
 		/* Skip Empty slots */
 		if (streq(buf,"(nothing)")) continue;
