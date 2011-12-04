9864fa37781225565e69f465310f10b5f42eea35
magnate <rantingman@gmail.com>
2011-12-04 22:03:05 +0000

    Correct bogus obj_is_ammo to kind_is_ammo.

--- src/object/obj-desc.c
+++ src/object/obj-desc.c
@@ -529,7 +529,7 @@ static size_t obj_desc_combat(const object_type *o_ptr, char *buf, size_t max,
 	/* Show weapon bonuses */
 	if (spoil || object_attack_plusses_are_visible(o_ptr)) {
 		if (wield_slot(o_ptr) == INVEN_WIELD || wield_slot(o_ptr) == INVEN_BOW
-				|| obj_is_ammo(o_ptr) || o_ptr->to_d || o_ptr->to_h) {
+				|| kind_is_ammo(o_ptr->tval) || o_ptr->to_d || o_ptr->to_h) {
 			/* Make an exception for body armor with only a to-hit penalty */
 			if (o_ptr->to_h < 0 && o_ptr->to_d == 0 &&
 			    (o_ptr->tval == TV_SOFT_ARMOR ||
